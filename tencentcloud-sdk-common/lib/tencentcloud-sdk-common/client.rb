42bb2c8cd6f402afe1115c0f4d0c4ca79c73a9ec
tencentcloudapi <tencentcloudapi@tenent.com>
2021-06-10 08:16:39 +0800

    release 1.0.92

--- tencentcloud-sdk-common/lib/tencentcloud-sdk-common/client.rb
+++ tencentcloud-sdk-common/lib/tencentcloud-sdk-common/client.rb
@@ -14,7 +14,7 @@ module TencentCloud
 
       @@api_version = ''
       @@endpoint = ''
-      @@sdk_version = '1.0.91'
+      @@sdk_version = '1.0.92'
       def initialize(credential, region, profile = nil)
         raise TencentCloudSDKException.new('InvalidCredential', 'Credential is None or invalid') unless credential
 
